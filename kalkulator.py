423ad59b69439254e8edade447f6a1b8ec7c47d8
reyna <reynamanansal026@student.unsrat.ac.id>
2025-11-24 15:26:37 +0800

    bagian fungsi operasi

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,20 @@
+# === KALKULATOR SEDERHANA ===
+# Kelompok J2R
+# Reyna Maselina Manansal 250211060092
+# Jesril Graciella Angela Mende 250211060017
+# Jessica Marsha Sangki 250211060061
 
+# Bagian Fungsi Operasi
+def tambah(a, b):
+    return a + b
+
+def kurang(a, b):
+    return a - b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    if b == 0:
+        return "Error: Tidak bisa membagi dengan nol!"
+    return a / b
